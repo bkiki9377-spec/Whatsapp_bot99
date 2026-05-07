8b69ce7280b024fa06ea6c85cc04a6d478b8a4e9
bkiki9377-spec <bkiki9377@gmail.com>
2026-05-07 23:30:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const {
+default: makeWASocket,
+useMultiFileAuthState,
+fetchLatestBaileysVersion
+} = require("@whiskeysockets/baileys");
+
+async function startBot() {
+
+const { state, saveCreds } = await useMultiFileAuthState("./session");
+const { version } = await fetchLatestBaileysVersion();
+
+const sock = makeWASocket({
+auth: state,
+version,
+printQRInTerminal: false
+});
+
+// حفظ الجلسة
+sock.ev.on("creds.update", saveCreds);
+
+// 🔑 Pairing Code بدل QR
+const phoneNumber = "212613415023"; // ضع رقمك هنا
+
+setTimeout(async () => {
+
+try {
+const code = await sock.requestPairingCode(phoneNumber);
+
+console.log("=================================");
+console.log("PAIRING CODE:", code);
+console.log("=================================");
+
+} catch (e) {
+console.log("فشل إنشاء كود الربط");
+}
+
+}, 3000);
+
+// 📩 استقبال الرسائل (بدون أوامر إضافية)
+sock.ev.on("messages.upsert", async ({ messages }) => {
+
+const msg = messages[0];
+if (!msg.message) return;
+
+const text =
+msg.message.conversation ||
+msg.message.extendedTextMessage?.text;
+
+if (!text) return;
+
+const from = msg.key.remoteJid;
+
+// رد بسيط فقط
+await sock.sendMessage(from, {
+text: "ان benchemmar بوت ، تم تطويري لتلبية حاجاتكم في الاجابة عن الاسئلة المتعلقة في جميع المجالات ، جرب الدردشة معي الان!!"
+});
+
+});
+
+}
+
+startBot();
